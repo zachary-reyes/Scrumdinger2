b40d7fa952a03ad18d84188b2cd54c87ab35ae27
ZARface <zacharyaustin@outlook.com>
2023-08-01 13:59:54 -0700

    error handling, async threads, & data store

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by Zachary Reyes on 8/1/23.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+    
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
