cbe3967492e0dc213fc79ef856499be07bf3f4d8
ZARface <zacharyaustin@outlook.com>
2023-08-01 14:21:53 -0700

    MeetingTimer Ring extracted to own view

---
+++ Scrumdinger/Views/SpeakerArc.swift
@@ -0,0 +1,20 @@
+//
+//  SpeakerArc.swift
+//  Scrumdinger
+//
+//  Created by Zachary Reyes on 8/1/23.
+//
+
+import SwiftUI
+
+struct SpeakerArc: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpeakerArc_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeakerArc()
+    }
+}
