3ed8b8b905700239814172b1dab75d63f6076bf3
ZARface <zacharyaustin@outlook.com>
2023-08-01 11:12:54 -0700

    added History to DetailView

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -11,6 +11,21 @@ struct MeetingView: View {
     
     private var player: AVPlayer {AVPlayer.sharedDingPlayer}
     
+    private func startScrum() {
+        scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+        scrumTimer.speakerChangedAction = {
+            player.seek(to: .zero)
+            player.play()
+        }
+        scrumTimer.startScrum()
+    }
+    
+    private func endScrum() {
+        scrumTimer.stopScrum()
+        let newHistory = History(attendees: scrum.attendees)
+        scrum.history.insert(newHistory, at: 0)
+    }
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
@@ -25,17 +40,10 @@ struct MeetingView: View {
         .padding()
         .foregroundColor(scrum.theme.accentColor)
         .onAppear {
-            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
-            scrumTimer.speakerChangedAction = {
-                player.seek(to: .zero)
-                player.play()
-            }
-            scrumTimer.startScrum()
+            startScrum()
         }
         .onDisappear {
-            scrumTimer.stopScrum()
-            let newHistory = History(attendees: scrum.attendees)
-            scrum.history.insert(newHistory, at: 0)
+            endScrum()
         }
         .navigationBarTitleDisplayMode(.inline)
     }
