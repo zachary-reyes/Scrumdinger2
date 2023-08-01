ee5cfce48a96ede6abdfe5e238b8402dd9bdaac4
ZARface <zacharyaustin@outlook.com>
2023-08-01 14:31:07 -0700

    create single arc segment representing a speaker's time

--- Scrumdinger/Views/SpeakerArc.swift
+++ Scrumdinger/Views/SpeakerArc.swift
@@ -7,14 +7,28 @@
 
 import SwiftUI
 
-struct SpeakerArc: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct SpeakerArc: Shape {
+    let speakerIndex: Int
+    let totalSpeakers: Int
+    
+    private var degreesPerSpeaker: Double {
+        360.0 / Double(totalSpeakers)
     }
-}
-
-struct SpeakerArc_Previews: PreviewProvider {
-    static var previews: some View {
-        SpeakerArc()
+    
+    private var startAngle: Angle {
+        Angle(degrees: degreesPerSpeaker * Double(speakerIndex) + 1.0)
+    }
+    
+    private var endAngle: Angle {
+        Angle(degrees: startAngle.degrees + degreesPerSpeaker - 1.0)
+    }
+ 
+    func path(in rect: CGRect) -> Path {
+        let diameter = min(rect.size.width, rect.size.height) - 24.0
+        let radius = diameter / 2.0
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        return Path { path in
+            path.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
+        }
     }
 }
